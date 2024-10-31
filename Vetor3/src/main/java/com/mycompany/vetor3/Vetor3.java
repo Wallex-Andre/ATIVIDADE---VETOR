ab3006f96a317e7ab0910efc77a284926b242ac5
Wallex Andre <wallexandre2009@gmail.com>
2024-10-30 22:19:21 -0300

    Projeto vetor

---
+++ Vetor3/src/main/java/com/mycompany/vetor3/Vetor3.java
@@ -0,0 +1,27 @@
+package com.mycompany.vetor3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Vetor3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[50];
+
+        System.out.println("Digite 50 números:");
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(numeros);
+
+        System.out.println("\nNúmeros em ordem crescente:");
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+        scanner.close();
+    }
+}
---
+++ Vetor4/src/main/java/com/mycompany/vetor4/Vetor4.java
@@ -0,0 +1,27 @@
+package com.mycompany.vetor4;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Vetor4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Integer[] numeros = new Integer[50];
+
+        System.out.println("Digite 50 números: ");
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(numeros, Collections.reverseOrder());
+         
+        System.out.println("\nNúmeros em ordem decrescente:");
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+        scanner.close();
+    }
+}
